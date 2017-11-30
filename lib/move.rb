6e40a7cc2400154fcd29ba3bc387ac0386f24e24
Ozan Araz <ozanaraz91@gmail.com>
2017-11-30 21:08:28 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,5 +8,8 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(input)
-  return board[Integer(input) - 1] = "X"
+  return input.to_i()
 end
+
+def move(board,index,player)
+  
